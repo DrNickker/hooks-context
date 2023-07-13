383b336a2ac1da9bcda2867e2c7ed2536ad8b1cc
DrNickker <mintagirov2015@yandex.ru>
2023-07-13 10:04:48 +0500

    css

--- src/Details.js
+++ src/Details.js
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react';
 export default function Details({data}) {
     return (
         <Fragment>
-            <div>
+            <div className="details">
                 <img src={data.avatar} alt="avatar"/>
                 <h2>{data.name}</h2>
                 <p>City: {data.details.city}</p>
